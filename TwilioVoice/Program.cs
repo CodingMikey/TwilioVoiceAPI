4b22f4f8657ee2579261760923d1c334f908b84d
Michael Galas <mgalas2019@hotmail.com>
2022-01-28 18:16:58 -0800

    importing twilio ref

--- TwilioVoice/Program.cs
+++ TwilioVoice/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace TwilioVoice
 {
@@ -10,7 +13,17 @@ namespace TwilioVoice
     {
         static void Main(string[] args)
         {
+            // Find your Account Sid and Auth Token at twilio.com/console
+            const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            const string authToken = "your_auth_token";
+            TwilioClient.Init(accountSid, authToken);
 
+            var to = new PhoneNumber("+14155551212");
+            var from = new PhoneNumber("+15017122661");
+            var call = CallResource.Create(to, from,
+                url: new Uri("http://demo.twilio.com/docs/voice.xml"));
+
+            Console.WriteLine(call.Sid);
         }
     }
 }
